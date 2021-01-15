539874723ac7c74cc5b1c9499e26161c6c818522
dongjuppp <dongjuppp@gmail.com>
2021-01-15 19:21:39 +0900

    docker

--- back/src/main/java/web/game/avalon/AvalonApplication.java
+++ back/src/main/java/web/game/avalon/AvalonApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AvalonApplication {
 
     public static void main(String[] args) {
+        System.out.println("실항한다!!!");
         SpringApplication.run(AvalonApplication.class, args);
     }
 
